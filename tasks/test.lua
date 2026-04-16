40fb5c0e00d50403e85ecd3d8343f9997d236f4c
Your Name <you@example.com>
2026-04-16 21:34:40 +1000

    feat: changed tasks

--- tasks/test.lua
+++ tasks/test.lua
@@ -3,6 +3,24 @@ local funcs = require("src.funcs")
 local st = m.singleTasks
 local wt = m.weeklyTask
 return {
+	wt("shave"),
+	wt("clean"),
+	wt("check emails"),
+	wt("cut nails"),
+	wt("clear tabs"),
+	wt("nix flake update"),
+	{
+		name = m.just("Humble Bundle"),
+		conditions = { m.isDayOfWeek(2), m.isWeekOfMonth(1, m.timedelta(0, -17)) },
+		-- -17 cause PST is 17 hours behind my time zone, its actually wednesday too
+		duetime = m.dueTime(m.timedelta(30)),
+		checkstart = function(date)
+			return funcs.floorToDay(date) - m.timedelta(31) + m.timedelta(0, 3)
+			-- 3 hours cause 3 am
+		end,
+		checkrepeats = m.justRepeats(2),
+		finishdelta = m.just(m.timedelta(0)),
+	},
 	st({
 		"Test works",
 		"bin scripts instead of hypr nixos",
@@ -30,23 +48,6 @@ return {
 		"Ctrl Backspace nvim",
 		"get logseq todo in this",
 		"message tax file number and supperannuation",
+		"pullup tower",
 	}),
-	wt("shave"),
-	wt("clean"),
-	wt("check emails"),
-	wt("cut nails"),
-	wt("clear tabs"),
-	wt("nix flake update"),
-	{
-		name = m.just("Humble Bundle"),
-		conditions = { m.isDayOfWeek(2), m.isWeekOfMonth(1, m.timedelta(0, -17)) },
-		-- -17 cause PST is 17 hours behind my time zone, its actually wednesday too
-		duetime = m.dueTime(m.timedelta(30)),
-		checkstart = function(date)
-			return funcs.floorToDay(date) - m.timedelta(31) + m.timedelta(0, 3)
-			-- 3 hours cause 3 am
-		end,
-		checkrepeats = m.justRepeats(2),
-		finishdelta = m.just(m.timedelta(0)),
-	},
 }
