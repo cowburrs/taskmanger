d2aa1b98900a1f99ef9a88cc06c9b1e69f9aa2ab
Your Name <you@example.com>
2026-04-17 13:14:54 +1000

    feat: changed tasks

--- tasks/school.lua
+++ tasks/school.lua
@@ -99,8 +99,6 @@ local returntable = {
 		"comp 5 final q",
 		"gitignore vesktop and fonts",
 		"make clear tabs task weekly",
-		"debloat computer",
-		"shellnix/devenvs",
 	}),
 	worksheetTasks("Lambda Calculus", dt(2026, 4, 4), timedelta(5), 8, {4, 6, 10})
 	-- textBookTasks("Jstweart", dt(2026, 4, 4), dt(2026, 5, 4), { 6, 8, 7, 5, 8, 6, 7, 8, 5, 9, 8, 8, 9 }),
